212bd84b3bca82c6b9911a93d17a81e69037b276
lee-insu <carpediem7760@gmail.com>
2021-03-15 22:40:02 +0900

    slide js

--- js/slideList.js
+++ js/slideList.js
@@ -1,3 +1,9 @@
 function transformPrev(e) {
-    const slidePrev = e.target
+    const slidePrev = e.target,
+          slideNext = slidePrev.nextElementSibling,
+          clothesList = slidePrev.previousElementSibling.children[1];
+
+    let activeLi = clothesList.getAttribute('data-position');
+
+    
 }
