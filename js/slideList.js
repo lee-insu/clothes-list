f4e65265d8635d79316b0bb56283f1f3b3cdcc19
lee-insu <carpediem7760@gmail.com>
2021-03-15 23:15:12 +0900

    slide js edit

--- js/slideList.js
+++ js/slideList.js
@@ -5,5 +5,46 @@ function transformPrev(e) {
 
     let activeLi = clothesList.getAttribute('data-position');
 
-    
+    if(Number(activeLi) < 0) {
+        activeLi = Number(activeLi) + 220;
+        slideNext.addEventListener('click',transformNext);
+        slideNext.classList.add('next-hover');
+
+        if(Number(activeLi) === 0) {
+            slidePrev.classList.remove('prev-hover');
+            slidePrev.removeEventLister('click',transformPrev);
+        }
+    }
+
+    clothesList.style.transition = 'transform 1s'
+    clothesList.style.transform = 'translateX('+ Number(activeLi) +'px)';
+    clothesList.setAttribute('data-position',activeLi);
+}
+
+
+function transformNext(e) {
+    const slideNext = e.target,
+          slidePrev = slideNext.previousElementSibling,
+          clothesList = slideNext.previousElementSibling.previousElementSibling.children[1],
+          liList = clothesList.getElementsByTagName('li');
+
+    let activeLi = clothesList.getAttribute('data-position');
+
+    if(clothesList.clientWidth < (liList.length * 355 + Number(activeLi))) {
+        activeLi = Number(activeLi) - 355;
+        slidePrev.classList.add('prev-hover');
+        
+        if(clothesList.clientWidth > (liList.length * 355 + Number(activeLi))) {
+            slideNext.removeEventListener('click',transformNext);
+            slideNext.classList.remove('next-hover');
+            
+        }
+        
+        slidePrev.addEventListener('click',transformPrev);
+    }
+
+    clothesList.style.transition = 'transform 1s'
+    clothesList.style.transform = 'translateX('+ Number(activeLi) +'px)';
+    clothesList.setAttribute('data-position',activeLi);
+
 }
